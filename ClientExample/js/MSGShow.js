3329f705a0157d53fa4d738d6689111e357c5962
dxyinme <dxyinme@outlook.com>
2020-12-18 12:48:02 +0800

    update chat icon

--- ClientExample/js/MSGShow.js
+++ ClientExample/js/MSGShow.js
@@ -5,13 +5,15 @@ class MSGShow {
 
     }
 
-    MsgBox(from, target, msg) {
-        return $("<div/>", {
-            "class": "chat-msg-box"
-        }).append(
-            $("<a/>", {
-                "text": msg
-            })
-        );
+    MsgBox(from, target, msg, isSelf, icon) {
+
+    }
+
+    Icon() {
+        return $('<img/>', {
+            "class": "chat-round-icon",
+            "alt": "",
+            "src": "https://avatars1.githubusercontent.com/u/32793868?s=400&u=2e2f7b7637470fff15afe82927532efb1a1fde5a&v=4"
+        });
     }
 }

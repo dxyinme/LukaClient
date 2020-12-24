6c569f57eff848fe6475b1d22d8924d6f94a4ae6
dxyinme <dxyinme@outlook.com>
2020-12-24 18:10:10 +0800

    update windows-client build

--- build.cmd
+++ build.cmd
@@ -1,3 +1,8 @@
-go build -o ./bin/client_cli.exe ./main/cli.go
-go build -o bin/cli_core.exe main/cli-core.go
-go build -o bin/gui.exe main/gui.go
+go build -o bin\gui.exe main\gui.go
+copy start.cmd bin\
+cd bin
+rd ClientExample
+mkdir ClientExample
+mkdir SaveTmp
+cd ..
+xcopy ClientExample bin\ClientExample /S/E
